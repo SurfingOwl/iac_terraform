5ca5d10cbdcf8da8f243d422f7f916c056e68390
QuentinCoqueran <coqueranquentin@gmail.com>
2023-12-07 21:59:48 +0100

    nginx conf

---
+++ infra/pods/nginx-config.tf
@@ -0,0 +1,9 @@
+resource "kubernetes_config_map" "nginx_frontend_website_config" {
+  metadata {
+    name = "nginx-frontend-website-config"
+  }
+
+  data = {
+    "default.conf" = file("${path.module}/config/nginx-website.conf")
+  }
+}
